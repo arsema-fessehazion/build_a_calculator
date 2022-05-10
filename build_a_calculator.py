d3344c1a625f8830d2c29ac3ca124a02da074bf8
Arsema Fessehazion <arsemafessehazion@gmx.com>
2022-05-10 12:42:41 +0100

    Initial commit

---
+++ build_a_calculator.py
@@ -0,0 +1,33 @@
+#Calculator
+
+def add(n1,n2):
+    return n1 + n2
+
+def subtract(n1,n2):
+    return n1 - n2
+
+def multiply(n1,n2):
+    return n1 * n2
+
+def divide(n1,n2):
+    return n1 / n2
+
+#store these functions in a dictionary
+#act as the means in which we will call the functions
+operations = {
+"+": add,
+"-": subtract,
+"*": multiply,
+"/":divide
+}
+
+num1 = int(input("What's the first number?: "))
+for symbol in operations:
+    print (symbol)
+    #only prints out keys in a dictionary
+operation_symbol = input("Pick an opertaion from the line above: ")
+num2 = int(input("What's the second number?: "))
+
+calculation_function = operations[operation_symbol]
+answer = calculation_function(num1,num2)
+print (f"{num1} {operation_symbol} {num2} = {answer}")
