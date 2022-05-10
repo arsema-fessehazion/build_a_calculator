ddaa3017742956e983543bf424558a15c76025f7
Arsema Fessehazion <arsemafessehazion@gmx.com>
2022-05-10 12:59:22 +0100

    updated

--- build_a_calculator.py
+++ build_a_calculator.py
@@ -29,5 +29,13 @@ operation_symbol = input("Pick an opertaion from the line above: ")
 num2 = int(input("What's the second number?: "))
 
 calculation_function = operations[operation_symbol]
-answer = calculation_function(num1,num2)
-print (f"{num1} {operation_symbol} {num2} = {answer}") 
+first_answer = calculation_function(num1,num2)
+print (f"{num1} {operation_symbol} {num2} = {first_answer}")
+
+# storethe previous answer to use in the next calculation
+operation_symbol = input("Pick an opertaion from the line above: ")
+num3 = int(input("What's the third number?: "))
+calculation_function = operations[operation_symbol]
+second_answer = calculation_function(first_answer,num3)
+print (f"{first_answer} {operation_symbol} {num3} = {second_answer}")
+# same way as saying answer = calculation_function((num1, num2),num3)
